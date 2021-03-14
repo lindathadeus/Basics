ce8a05d14aeb16e6e5f26d946f0637b13ce95c03
Linda Thadeus <linda.thadeus@broadcom.com>
2021-03-14 08:56:27 -0700

    majoriy element partial

--- merge_sort/leetcode/majority_element.py
+++ merge_sort/leetcode/majority_element.py
@@ -1,7 +1,7 @@
 """
 Given an array nums of size n, return the majority element.
 
-The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+The majority element appears more [n/2] times. It always exists in the testcases
 
 Input: nums = [3,2,3]
 Output: 3
@@ -19,27 +19,53 @@ n == nums.length
 Follow-up: Could you solve the problem in linear time and in O(1) space?
 """
 
-A = [3,2,3]
-print(A)
-C = {}
+A = [2, 1, 2, 1, 2]
+
+U = {}
+
+def majority_element():
+  return U
+  for i in U:
+    if U[i] >= len(A)/2:
+      return U[i]
 
 def merge(L, R):
-    i = 0
-    j = 0
+    for i in L:
+        if i in U:
+          U[i] += 1
+        else:
+          U[i] = 1
+      
+    for i in R:
+        if i in U:
+          U[i] += 1
+        else:
+          U[i] = 1
+      
+    return U
 
-    
-
-def majority_element(A):
+def divide(A): #updates U
     #base case
-    if len(A) == !:
-        C[A[0]] = 1        
-        return A[0]
+    if len(A) <= 1:
+        if A[0] in U:
+          U[A[0]] += 1
+        else:
+          U[A[0]] = 1
+        return A
+    
+    #divide by 2
+    mid = len(A) // 2 #floor of middle
+    L = A[:mid] #L = A[0..mid-1]
+    R = A[mid:] #R = A[mid..n-1], n is the size of A
 
-    mid = len(A) // 2
-    L = A[:mid] #0 to mid - 1
-    R = A[mid:] #mid to len(A) - 1
+    #recurse
+    L = divide(L) #divide the given list
+    R = divide(R)
 
-    L = majority_element(L)
-    R = majority_element(R)
+    #merge and return
+    merge(L, R)
+    return majority_element()
 
-    return merge(L, R)
+print(A)
+A=divide(A)
+print(A)
