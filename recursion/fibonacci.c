20a26b17dfc3f95357ed04ef6747d223f01cfd7b
Linda Thadeus <lindathadeus@gmail.com>
2021-09-11 19:37:49 +0530

    added memo inside recursion

--- recursion/fibonacci.c
+++ recursion/fibonacci.c
@@ -1,12 +1,11 @@
 /****************************************************************
-* File name     : memoized_fibonacci.c
+* File name     : fibonacci.c
 * Purpose       :
 * Author        : Linda J
 * Creation date : 10-09-2021
 ****************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 #define DEBUG
 #define FATAL    (1)
@@ -24,8 +23,9 @@ unsigned int  debug_level = USR;
 #define printd(level, ...)
 #endif
 
-unsigned int memoized_fib(unsigned int n);
+unsigned int iterative_fib(unsigned int n);
 unsigned int naive_fib(unsigned int n);
+unsigned int memoized_fib(unsigned int n, int *memo);
 
 unsigned int naive_fib(unsigned int n) {
   if (n <= 2)
@@ -33,29 +33,42 @@ unsigned int naive_fib(unsigned int n) {
   return naive_fib(n - 1) + naive_fib(n - 2);
 }
 
-//memoized
-unsigned int memoized_fib(unsigned int n) {
+unsigned int iterative_fib(unsigned int n) {
   unsigned int fib[n+1];
 
-  fib[0] = 0;
-  fib[1] = 1;
-  fib[2] = 1;
-
-  for (unsigned int i = 3; i <= n; i++) {
-    fib[i] = fib[i -1] + fib[i - 2]; 
-  }
+  for (unsigned int i = 0; i <= 2; i++)
+    fib[i] = 1;
+  
+  for (unsigned int i = 3; i <= n; i++)
+    fib[i] = fib[i - 1] + fib[i - 2];
   
   return fib[n];
 }
 
+//memoized
+unsigned int memoized_fib(unsigned int n, int *memo) {
+  
+  //check if n is present in memo, if yes, then return that
+  for (int i = 0; i < n; i++)
+    if (i == n)
+      if (memo[i] != -1)
+        return memo[i];
+
+  if (n <= 2)
+    return 1;
+
+  memo[n] =  memoized_fib(n - 1, memo) + memoized_fib(n - 2, memo);
+  return memo[n];
+}
+
 int main(unsigned int argc, char *argv[], char *envp[]) {
-  unsigned int n;
-  printd(USR, "\tfibonacci\n\n");
+  unsigned int n, memo[50] = {-1};
   
   for(unsigned int i = 1; i < argc; i++) {
     n = atoi(argv[i]);
-    printf("memoized_fib(%d) = %d\n", n, memoized_fib(n));
-    printf("native_fib(%d) = %d\n", n, naive_fib(n));
+    printf("iterative_fib(%d) = %d\n", n, iterative_fib(n));
+    printf("memoized_fib(%d) = %d\n", n, memoized_fib(n, memo));
+    //printf("native_fib(%d) = %d\n", n, naive_fib(n));
   }
   return 0;
 }
