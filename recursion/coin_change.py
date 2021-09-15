c295187c2cde987893eced3560030cbdaf7ae411
Linda Thadeus <lindathadeus@gmail.com>
2021-09-15 21:29:56 +0530

    coin change rec solution from leetcode

--- recursion/coin_change.py
+++ recursion/coin_change.py
@@ -11,18 +11,30 @@ import sys
 def coin_change(bill, lst):
     print("")
 
-def combinationSum4(nums, target):
-  nums, combs = sorted(nums), [1] + [0] * (target)
-  for i in range(target + 1): 
-      for num in nums:
-          if num  > i: break
-          if num == i: combs[i] += 1
-          if num  < i: combs[i] += combs[i - num]
-  return combs[target]
+def combinationSum(nums, target):
+    if (target == 0):
+        return 1;
+    
+    res = 0;
+    for i in range(len(nums)): 
+        if (target >= nums[i]):
+            res += combinationSum4(nums, target - nums[i]);
+    
+    return res;
+
+def combinationSum4(nums, N):
+  nums, comb_sum = sorted(nums), [1] + [0] * (N)
+  for i in range(N + 1): 
+      for x in nums:
+          if x  > i: break
+          if x == i: comb_sum[i] += 1
+          if x  < i: comb_sum[i] += comb_sum[i - x]
+  return comb_sum[N]
 
 def main():
     print (combinationSum4([1,2,3], 4))
     print (combinationSum4([1,2,3], 0))
+    print (combinationSum([1,2,3], 4))
     return 0
 
 if __name__ == "__main__":
