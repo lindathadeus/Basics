3361394e7a91e5077976413e21408f328ba5894c
Linda Thadeus <linda.thadeus@broadcom.com>
2021-11-11 23:01:37 -0800

    min_steps_1 elegant

--- dp/min_steps_1.c
+++ dp/min_steps_1.c
@@ -17,15 +17,28 @@ Now the question is, given a positive integer n, find the minimum number of step
 #define minimum(X, Y, Z) min(min(X, Y), Z)
 
 int min_steps_1(int n) {
+  if (n == 1)
+    return 0;
+
+  int steps = min_steps_1(n - 1);
+  if (n % 3 == 0)
+    steps = min(steps, min_steps_1(n / 3));
+  if (n % 2 == 0)
+    steps = min(steps, min_steps_1(n / 2));
+
+  return 1 + steps;
+}
+
+int min_steps_1_old(int n) {
   if (n > 1) {
     if ((n % 6 == 0))
-      return 1 + minimum(min_steps_1(n/2), min_steps_1(n/3), min_steps_1(n - 1));
+      return 1 + minimum(min_steps_1_old(n/2), min_steps_1_old(n/3), min_steps_1_old(n - 1));
     else if (n % 3 == 0)
-      return 1 + min(min_steps_1(n/3), min_steps_1(n - 1));
+      return 1 + min(min_steps_1_old(n/3), min_steps_1_old(n - 1));
     else if (n % 2 == 0)
-      return 1 + min(min_steps_1(n/2), min_steps_1(n - 1));
+      return 1 + min(min_steps_1_old(n/2), min_steps_1_old(n - 1));
     else
-      return 1 + min_steps_1(n - 1);
+      return 1 + min_steps_1_old(n - 1);
 	}
   return 0;
 }
@@ -56,6 +69,7 @@ int main(int argc, char *argv[], char *envp[]) {
   printf("%d\n", min_steps_1(2));
   printf("%d\n", min_steps_1(3));
   printf("%d\n", min_steps_1(17));
+  printf("%d\n", min_steps_1(10));
   printf("\n%d\n", min_steps_1_dp(17));
   printf("%d\n", min_steps_1_dp(3));
   printf("%d\n", min_steps_1_dp(2));
